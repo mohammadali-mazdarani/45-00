f5d41755c2a8230bbfb1d21a7ffdf29d1c05173c
Mohammad ali mazdarani <74422713+mohammadali-mazdarani@users.noreply.github.com>
2025-01-15 10:10:17 -0800

    Docs : description of program in practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -4,7 +4,10 @@
 using namespace std;
 /* run this program using the console pauser or add your own getch, system(
 ) or input loop */
-/* Description of program*/
+/* 
+* Description of program :
+* Cascading member function calls with the this pointer
+*/
 //create namespace
 /*Branch name
 *
