abaea6a051c07173a55afb8ca7f888710e780de8
Mohammad ali mazdarani <74422713+mohammadali-mazdarani@users.noreply.github.com>
2025-01-15 10:07:21 -0800

    Build : Build C++ file

---
+++ practice.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system(
+) or input loop */
+/* Description of program*/
+//create namespace
+/*Branch name
+*
+*
+*/
+//prototype
+//--------------------------
+//micro
+//---------------------
+//global variable
+//-------------------
+//struct
+//-------------------
+//union
+//-------------------
+//class
+//-------------------
+int main(int argc, char** argv)
+{
+
+getch();
+}
+//function variable
+//--------------------------
+//functions
